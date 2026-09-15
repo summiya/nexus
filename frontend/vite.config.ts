1869938cc9b6ad458680d9a1a433c502669870a6
Summiya <summiya.rasheed@gmail.com>
2026-09-15 16:42:52 +0400

    NEXUS-111: configure Vitest with Vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite';
+/// <reference types="vitest/config" />
+
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   envDir: '..',
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    css: true,
+  },
 });
